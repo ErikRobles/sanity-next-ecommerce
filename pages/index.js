bb91b0712fc1c27c1b776298d1c7f91de6e2ffc4
ErikRobles <erikjames69@hotmail.com>
2022-09-27 07:56:59 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import { Typography } from '@mui/material'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazona</title>
+        <meta name="description" content="Simplified Sanity E-commerce" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <Typography component="h1" variant="h1">Sanity Amazona</Typography>
+     
+    </div>
+  )
+}
